7c5335f3b3c9b6340b8de0d60a0d1b111585774b
yydschenyi <1512952601@qq.com>
2024-04-09 23:52:40 +0800

    update

--- exercises/intro/intro2.rs
+++ exercises/intro/intro2.rs
@@ -5,8 +5,9 @@
 // Execute `rustlings hint intro2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 fn main() {
-    println!("Hello {}!");
+    let m="world";
+    println!("Hello {}!",m);
 }
