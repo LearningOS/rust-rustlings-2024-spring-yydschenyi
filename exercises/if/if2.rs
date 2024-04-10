6ce4110c9b51c430bddbc0ff2abe689632e585a0
yydschenyi <1512952601@qq.com>
2024-04-10 13:06:35 +0800

    s

--- exercises/if/if2.rs
+++ exercises/if/if2.rs
@@ -5,13 +5,16 @@
 //
 // Execute `rustlings hint if2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
-pub fn foo_if_fizz(fizzish: &str) -> &str {
+
+pub fn foo_if_fizz(fizzish: &str) -> String {
     if fizzish == "fizz" {
-        "foo"
-    } else {
-        1
+        "foo".to_string()
+    } else if fizzish=="fuzz"{
+        "bar".to_string()
+    }
+    else {
+        "baz".to_string()
     }
 }
 
